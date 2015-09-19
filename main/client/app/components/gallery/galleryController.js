1eeceec2a687333c34cfbc997879e4dd680c0083
Gregory Blake <greg@gregoryblake.net>
2015-09-18 19:39:08 -0700

    Added images to gallery view. Task for Tomorrow: Fix and complete the Landing Page.

--- main/client/app/components/gallery/galleryController.js
+++ main/client/app/components/gallery/galleryController.js
@@ -1,7 +1,55 @@
-app.controller('GalleryController', ['$scope', 'photos', function($scope, photos) {
-  photos.success(function(data) {
-    $scope.photos = data;
-  });
+app.controller('GalleryController', ['$scope', function($scope) {
+  $scope.photos = [
+                     {
+                    	 "title": "Barn",
+                    	 "thumbnailUrl": "/assets/img/art/BarnThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/BarnFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Daytime Quebec",
+                    	 "thumbnailUrl": "/assets/img/art/DaytimeQuebecThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/DaytimeQuebecFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Nightly Quebec",
+                    	 "thumbnailUrl": "/assets/img/art/NightlyQuebecThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/NightlyQuebecFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Storm Clouds",
+                    	 "thumbnailUrl": "/assets/img/art/StormCloudsFullSize.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/StormCloudsFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Jingwei",
+                    	 "thumbnailUrl": "/assets/img/art/JingweiThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/JingweiFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Tiger Lily",
+                    	 "thumbnailUrl": "/assets/img/art/TigerLilyThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/TigerLilyFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2010"
+                     },
+                     {
+                    	 "title": "Neptune",
+                    	 "thumbnailUrl": "/assets/img/art/NeptuneThumbnail.jpg",
+                    	 "fullSizeUrl": "/assets/img/art/NeptuneFullSize.jpg",
+                    	 "medium": "Digital Painting",
+                    	 "year": "2014"
+                     }
+                  ];
   $scope.photoClicked = {};
   $scope.overlayShowing = false;
   $scope.displayOverlay = function(photo) {
