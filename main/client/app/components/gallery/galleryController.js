51e9ca6a037abbb9d604a557880ec55f47686991
Gregory Blake <greg@gregoryblake.net>
2015-09-23 18:59:07 -0700

    Right and left arrow buttons. TOMORROW: Clean up the code in the Controller, it's ugly. Possibly add right and left arrows to the overlay.

--- main/client/app/components/gallery/galleryController.js
+++ main/client/app/components/gallery/galleryController.js
@@ -2,18 +2,36 @@ app.controller('GalleryController', ['$scope', '$routeParams', 'galleryService',
 	$scope.photoClicked = {};
 	$scope.overlayShowing = false;
 	  
-	$scope.displayOverlay = function(image) {
-		$scope.photoClicked = image;
+	$scope.displayOverlay = function(index) {
+		$scope.photoClicked = $scope.gallery.images[index];
+		$scope.index = index;
 		$scope.overlayShowing = true;
 	};
 	$scope.hideOverlay = function() {
 		$scope.photoClicked = {};
 		$scope.overlayShowing = false;
 	};
+	
 	$scope.pressEsc = function(e){
 		var ESC_KEY = 27;
+		var LEFT_KEY = 37;
+		var RIGHT_KEY = 39;
 		if (e.which === ESC_KEY){
 			$scope.hideOverlay();
+		} else if (e.which === LEFT_KEY) {
+			if ($scope.index === 0){
+				$scope.index = $scope.gallery.images.length - 1;
+			} else {
+				$scope.index--;
+			}
+			$scope.photoClicked = $scope.gallery.images[$scope.index];
+		} else if (e.which === RIGHT_KEY) {
+			if ($scope.index === $scope.gallery.images.length - 1){
+				$scope.index = 0;
+			} else {
+				$scope.index++;
+			}
+			$scope.photoClicked = $scope.gallery.images[$scope.index];
 		}
 	}
 
