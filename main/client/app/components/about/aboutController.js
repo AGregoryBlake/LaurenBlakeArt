2650098f9b5d34937b86d07578a46012f5376ad0
Gregory Blake <greg@gregoryblake.net>
2015-09-26 16:16:27 -0700

    Added About text. TOMORROW: Store? Contact? Footer buttons?

---
+++ main/client/app/components/about/aboutController.js
@@ -0,0 +1,7 @@
+app.controller('AboutController', ['$scope', function($scope) {
+	$scope.artistPhoto = "assets/img/Lauren.jpg";
+	$scope.title = "About";
+	$scope.line1 = "Lauren was born and raised in New Orleans. After she earned her BS in Biology at Louisiana State University, she drove across the country lured by Bigfoot's siren song.";
+	$scope.line2 = "Interested in art since she was a child, she decided that she'd rather make pretty things than analyze sewage cultures.";
+	$scope.line3 = "Lauren is a second year student in Georgetown Atelier's full-time program and lives in Seattle with her husband, Greg.";
+}]);
