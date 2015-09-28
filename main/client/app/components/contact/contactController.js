4db9e39cbfafe24055afcbe3e0e263678802aab5
Gregory Blake <greg@gregoryblake.net>
2015-09-27 20:59:22 -0700

    Began work on Contact Form. Leaving it here for now.

--- main/client/app/components/contact/contactController.js
+++ main/client/app/components/contact/contactController.js
@@ -1,3 +1,21 @@
 app.controller('ContactController', ['$scope', function($scope) {
+	$scope.contact = {
+			fname: "",
+			lname: "",
+			email: "",
+			subject: "",
+			message: ""
+	}
+	$scope.error = {
+			fname: false,
+			lname: false,
+			email: false,
+			subject: false,
+			message: false,
+			submit: false
+	}
+	
+	$scope.send = function(contact) {
 
+	}
 }]);
