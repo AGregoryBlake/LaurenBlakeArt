100174d5c6035f0de41d9002878594c35d1c2366
Gregory Blake <greg@gregoryblake.net>
2015-09-28 18:38:59 -0700

    Created Image Service, which Gallery Service uses to acquire showpieces, and Store Service uses to display products. TOMORROW: Store? Contact? Footer images?

--- main/client/app/components/gallery/galleryService.js
+++ main/client/app/components/gallery/galleryService.js
@@ -1,119 +1,21 @@
-app.factory('galleryService', [function() {
-	galleryService = {};
-
-	var conceptArt = {
-			galleryName: "Concept Art",
-	        images: [
-	                 {
-	                	 "title": "Neptune",
-	                	 "thumbnailUrl": "/assets/img/art/NeptuneThumbnail.jpg",
-	                	 "fullSizeUrl": "/assets/img/art/NeptuneFullSize.jpg",
-	                	 "medium": "Digital Painting",
-	                	 "year": "2014"
-	                 }
-	                ]
+app.factory('galleryService', ['imageService', function(imageService) {
+	var findGallery = function(galleryName) {
+		var gallery = {};
+		gallery.galleryName = galleryName;
+		gallery.images = imageService.getImagesByGallery(galleryName);
+		return gallery;
 	};
-	var illustration = {
-			galleryName: "Illustration",
-			images: [
-			         {
-			        	 "title": "Barn",
-			        	 "thumbnailUrl": "/assets/img/art/BarnThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/BarnFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Daytime Quebec",
-			        	 "thumbnailUrl": "/assets/img/art/DaytimeQuebecThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/DaytimeQuebecFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Nightly Quebec",
-			        	 "thumbnailUrl": "/assets/img/art/NightlyQuebecThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/NightlyQuebecFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Storm Clouds",
-			        	 "thumbnailUrl": "/assets/img/art/StormCloudsFullSize.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/StormCloudsFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Jingwei",
-			        	 "thumbnailUrl": "/assets/img/art/JingweiThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/JingweiFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Tiger Lily",
-			        	 "thumbnailUrl": "/assets/img/art/TigerLilyThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/TigerLilyFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2010"
-			         },
-			         {
-			        	 "title": "Neptune",
-			        	 "thumbnailUrl": "/assets/img/art/NeptuneThumbnail.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/NeptuneFullSize.jpg",
-			        	 "medium": "Digital Painting",
-			        	 "year": "2014"
-			         }
-			        ]
-		};
-	var fineArt = {
-			galleryName: "Fine Art",
-			images: [
-			         {
-			        	 "title": "Hand and Ball",
-			        	 "thumbnailUrl": "/assets/img/art/fineart/handball.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/fineart/handball.jpg",
-			        	 "medium": "Charcoal",
-			        	 "year": "2015"
-			         },
-			         {
-			        	 "title": "Richard",
-			        	 "thumbnailUrl": "/assets/img/art/fineart/richard.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/fineart/richard.jpg",
-			        	 "medium": "Charcoal",
-			        	 "year": "2015"
-			         },
-			         {
-			        	 "title": "Chelsea",
-			        	 "thumbnailUrl": "/assets/img/art/fineart/chelsea.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/fineart/chelsea.jpg",
-			        	 "medium": "Charcoal",
-			        	 "year": "2015"
-			         },
-			         {
-			        	 "title": "Serenade",
-			        	 "thumbnailUrl": "/assets/img/art/fineart/serenade.jpg",
-			        	 "fullSizeUrl": "/assets/img/art/fineart/serenade.jpg",
-			        	 "medium": "Charcoal",
-			        	 "year": "2015"
-			         }
-			        ]
-	};
-	var error = {
-			galleryName: "Error: Gallery Not Found",
-			images: []
-	};
-	galleryService.assignGallery = function(id){
-		if(id == 'concept-art') {
-			return conceptArt;
-		} else if(id == 'illustration'){
-			return illustration;
-		} else if (id == 'fine-art') {
-			return fineArt;
-		} else {
-			return error;
+	return {
+		assignGallery: function(id){
+			if(id == 'concept-art') {
+				return findGallery("Concept Art");
+			} else if(id == 'illustration'){
+				return findGallery("Illustration");
+			} else if (id == 'fine-art') {
+				return findGallery("Fine Art");
+			} else {
+				return findGallery("Error: Gallery Not Found");
+			}
 		}
-	};
-	return galleryService;
+	}
 }]);
