7f84d549b5b8edb25fd8765ba19df3eb415b3388
Gregory Blake <KenseiMaedhros@users.noreply.github.com>
2016-02-29 15:41:19 -0800

    Duplicate client's imageService data.

---
+++ main/server/routes/images.js
@@ -0,0 +1,118 @@
+var app = require('../app');
+
+var images = [
+				{
+					"title" : "Neptune",
+					"thumbnailUrl" : "/assets/img/art/NeptuneThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/NeptuneFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2014",
+					"gallery" : "Concept Art",
+					"forSale" : false
+				},
+				{
+					"title" : "Barn",
+					"thumbnailUrl" : "/assets/img/art/BarnThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/BarnFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Daytime Quebec",
+					"thumbnailUrl" : "/assets/img/art/DaytimeQuebecThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/DaytimeQuebecFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Nightly Quebec",
+					"thumbnailUrl" : "/assets/img/art/NightlyQuebecThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/NightlyQuebecFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Storm Clouds",
+					"thumbnailUrl" : "/assets/img/art/StormCloudsFullSize.jpg",
+					"fullSizeUrl" : "/assets/img/art/StormCloudsFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Jingwei",
+					"thumbnailUrl" : "/assets/img/art/JingweiThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/JingweiFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Tiger Lily",
+					"thumbnailUrl" : "/assets/img/art/TigerLilyThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/TigerLilyFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2010",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Neptune",
+					"thumbnailUrl" : "/assets/img/art/NeptuneThumbnail.jpg",
+					"fullSizeUrl" : "/assets/img/art/NeptuneFullSize.jpg",
+					"medium" : "Digital Painting",
+					"year" : "2014",
+					"gallery" : "Illustration",
+					"forSale" : false
+				},
+				{
+					"title" : "Hand and Ball",
+					"thumbnailUrl" : "/assets/img/art/fineart/handball.jpg",
+					"fullSizeUrl" : "/assets/img/art/fineart/handball.jpg",
+					"medium" : "Charcoal",
+					"year" : "2015",
+					"gallery" : "Fine Art",
+					"forSale" : false
+				},
+				{
+					"title" : "Richard",
+					"thumbnailUrl" : "/assets/img/art/fineart/richard.jpg",
+					"fullSizeUrl" : "/assets/img/art/fineart/richard.jpg",
+					"medium" : "Charcoal",
+					"year" : "2015",
+					"gallery" : "Fine Art",
+					"forSale" : true
+				},
+				{
+					"title" : "Chelsea",
+					"thumbnailUrl" : "/assets/img/art/fineart/chelsea.jpg",
+					"fullSizeUrl" : "/assets/img/art/fineart/chelsea.jpg",
+					"medium" : "Charcoal",
+					"year" : "2015",
+					"gallery" : "Fine Art",
+					"forSale" : true,
+					"price" : 300
+				},
+				{
+					"title" : "Serenade",
+					"thumbnailUrl" : "/assets/img/art/fineart/serenade.jpg",
+					"fullSizeUrl" : "/assets/img/art/fineart/serenade.jpg",
+					"medium" : "Charcoal",
+					"year" : "2015",
+					"gallery" : "Fine Art",
+					"forSale" : true,
+					"price" : 300
+				}
+			];
+
+app.get('/images', function (req, res, next) {
+	res.body = JSON.stringify(images);
+});
