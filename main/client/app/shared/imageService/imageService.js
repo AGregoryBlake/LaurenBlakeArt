b45289c9492e59df0daf7a14297dc081488dbcf9
Gregory Blake <greg@gregoryblake.net>
2016-02-29 18:10:06 -0800

    Fixing errors from github untested checkin.

--- main/client/app/shared/imageService/imageService.js
+++ main/client/app/shared/imageService/imageService.js
@@ -1,20 +1,20 @@
 app.service('imageService', ['$resource', function($resource) {
-  var Images = $resource('/images');
-	var images = Images.get();
-	
-	return {
-		getAllImages: function() {
-			return images;
-		},
-		getImagesByGallery: function(gallery) {
-			return images.filter(function(image) {
-				return image.gallery === gallery;
-			});
-		},
-		getImagesForSale: function() {
-			return images.filter(function(image) {
-				return image.forSale;
-			});
-		}
-	};
+    var Images = $resource('/images');
+    var images = Images.query();
+    
+    return {
+	getAllImages: function() {
+	    return images;
+	},
+	getImagesByGallery: function(gallery) {
+	    return images.filter(function(image) {
+		return image.gallery === gallery;
+	    });
+	},
+	getImagesForSale: function() {
+	    return images.filter(function(image) {
+		return image.forSale;
+	    });
+	}
+    };
 }]);
